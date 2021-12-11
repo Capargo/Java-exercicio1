aca467ee2b3564311cab819041ddc055b5a3211b
caroline parra gomes <Cargomes@server01.foursys.local>
2021-12-11 16:45:27 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.lang.model.util.SimpleElementVisitor14;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Crie um programa que apresente seu nome seu celular e sua cidade.
+
+        String nome;
+        String celular;
+        String cidade;
+
+                nome = "Caroline Parra Gomes";
+                celular = "(11)99808-7812";
+                cidade = "Santana de Parnaiba";
+
+                System.out.println(nome);
+                System.out.println(celular);
+                System.out.print(cidade);
+
+                
+    }
+}
